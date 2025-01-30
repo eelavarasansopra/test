9b8de51705441cdd23582c67c4b1083be54db1e3
Elavarasan <Elavarasan.AP1@tesco.com>
2025-01-30 15:42:45 +0530

    cespm-1234 debug

--- src/main/java/com/myspace/config/SecurityConfig.java
+++ src/main/java/com/myspace/config/SecurityConfig.java
@@ -35,6 +35,9 @@ public class SecurityConfig {
 	    // Configuring HttpSecurity
 	    @Bean
 	    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+
+			System.out.println("test");
 	        return http.csrf(csrf -> csrf.disable())
 	                .authorizeHttpRequests(auth -> auth.requestMatchers("/auth/welcome", "/auth/addNewUser", "/auth/generateToken").permitAll())
 	                .authorizeHttpRequests(auth -> auth.requestMatchers("/auth/user/**","/auth/admin/**").authenticated())
